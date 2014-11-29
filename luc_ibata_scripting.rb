42bf4aa8afc027a6d8b96ad6a6549e718a3e7a82
Luc Walter iBata <ibata@wondesire-lm.corp.yahoo.com>
2014-11-29 13:53:17 -0800

    updating  files

--- luc_ibata_scripting.rb
+++ luc_ibata_scripting.rb
@@ -1,11 +1,14 @@
+#Begin Timer
+beginning_time = Time.now
+(1..10000).each { |i| i }
 # by luc iBata aka Speed Matters  
 # twitter @lucibata
 # done 1 line or 6 lines
 
 =begin
-I  have a one liner but because it does not display hidden files, 
-I am using the following code that iterates, 
-using the "do" command, through files found  by the "find" module
+I  have a one liner but because it does not show hidder I am using the following code that iterates, 
+using "do", looping through files found  by the "find" module.
+I would pick the one liner just because it runs faster (performance)
 =end
 
 require 'find'
@@ -15,5 +18,9 @@ Find.find(directory_name).each do |filefound|
 	puts filefound	
 end
 
-#One liner
-#puts Dir['**/*']
+#One liner code below
+#puts Dir['**/*'] 
+
+#End Timer
+end_time = Time.now
+puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
